e0031a579ec6aeabbb879e6c15227c7dc5d8aa4d
DeadDude-glitch <ahmed.ussef248@gmail.com>
2024-04-05 00:08:14 +0200

    explain the compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# -x sets language to C++
-#          -o output the compiled code to a file named rawcat
-#                    -I include the rawcat header files
-# 				   files to compile
+#   | sets language to C++
+#   |      | output the compiled code to a file named rawcat
+#   |      |         | include the rawcat header files
+#   |      |         |             
 g++ -x c++ -o rawcat -I ./include/ src/main.cpp src/rawcat.cpp
+#                                  |            |
+#                                  | source files to compile
