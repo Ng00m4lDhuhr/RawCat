c6aed9103637cdada1169b656421516d2a3c5bb7
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-05-02 17:04:26 +0300

    Delete src directory

--- src/rawcat.py
+++
@@ -1,6 +0,0 @@
-import network
-
-if __name__ == '__main__':
-    blabla = b"I do not know what I am doing but I am not hacking"
-    network.transmit("8.8.8.8", blabla)
-    print("seems like no error is there.")
